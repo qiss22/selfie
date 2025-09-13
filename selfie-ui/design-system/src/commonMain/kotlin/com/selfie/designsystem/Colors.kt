cde2a891eb4f83754a433479b62afd8aeeff6738
qiss22 <neoqiss22@gmail.com>
2025-09-14 01:59:12 +0200

    Update Colors.kt

--- selfie-ui/design-system/src/commonMain/kotlin/com/selfie/designsystem/Colors.kt
+++ selfie-ui/design-system/src/commonMain/kotlin/com/selfie/designsystem/Colors.kt
@@ -1,19 +1,14 @@
-package com.selfie.designsystem
+package design.tokens
 
 import androidx.compose.ui.graphics.Color
 
 object SelfieColors {
-    // Light theme
-    val Primary = Color(0xFF007AFF)
-    val Secondary = Color(0xFF5856D6)
-    val Background = Color(0xFFFFFFFF)
-    val Surface = Color(0xFFF2F2F7)
-    val Error = Color(0xFFFF3B30)
-    
-    // Dark theme
-    val PrimaryDark = Color(0xFF0A84FF)
-    val SecondaryDark = Color(0xFF5E5CE6)
-    val BackgroundDark = Color(0xFF000000)
-    val SurfaceDark = Color(0xFF1C1C1E)
-    val ErrorDark = Color(0xFFFF453A)
+    val PrimaryBrown = Color(0xFF2F221A)
+    val AccentGray = Color(0xFFD3D0CD)
+    val LightGray = Color(0xFFE8E6E6)
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+
+    // Optional: stronger accent (suggested improvement)
+    val AccentTeal = Color(0xFF3ABAB4)
 }
